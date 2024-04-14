6b71b72b8a6822a03c18e5320b9570fa96569205
ax0907 <aloha.aoto0907@gmail.com>
2024-04-14 15:44:05 +0900

    Update application

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   #default from: "from@example.com"
-  default from: 'restaurantaplication.heroku.com'
+  #default from: 'restaurantaplication.heroku.com'
+  default from: "aloha.aoto0907@gamil.com"
   layout "mailer"
 end
