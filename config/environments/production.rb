d8df80623474bf22f70002479a068175fd6e9804
ax0907 <aloha.aoto0907@gmail.com>
2024-04-21 12:02:02 +0900

    #21

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,18 +81,32 @@ Rails.application.configure do
   #メール設定
   config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  #ActionMailer::Base.smtp_settings = {
+  #  :port           => ENV['MAILGUN_SMTP_PORT'],
+  #  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  #  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  #  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     #:domain         => 'restaurantaplication.heroku.com',
-    :domain         => 'restaurantaplication-85826252b2fb.herokuapp.com',
-    :authentication => :plain,
-  }
+ #   :domain         => 'restaurantaplication-85826252b2fb.herokuapp.com',
+  #  :authentication => :plain,
+  #}
   #config.action_mailer.default_url_options = { host: 'restaurantaplication.heroku.com'}
   config.action_mailer.default_url_options = { host: 'restaurantaplication-85826252b2fb.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    #gmail利用時はaddress,domain,portは下記で固定
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    #gmailのユーザアカウント（xxxx@gmail.com)※念のため、credentials.yml.enc行き
+    user_name: Rails.application.credentials.user_name,
+    #gmail２段階認証回避のためにアプリケーションでの利用パスワードを取得、必ずcredentials.yml.endに設定を！！
+    password: Rails.application.credentials.password,
+    #パスワードをBase64でエンコード
+    authentication: :login
+  }
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
