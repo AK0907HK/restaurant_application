58a0e1e6e8bb4144e7bd751a1b75035354ff7888
ax0907 <aloha.aoto0907@gmail.com>
2025-01-13 17:32:35 +0900

    change restaurant display

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,19 +1,24 @@
 class Restaurant < ApplicationRecord
   belongs_to :user 
-  has_one_attached :image
+  #has_one_attached :image
+  has_many_attached :images 
   #has_many: posts
   default_scope -> { order(created_at: :desc) }
   validates :name, presence: true
   #validates :user_r_id, presence: true
   validates :area1, presence: true
-  #validates :area2, presence: true
-  #validates :type
-  #validates :time
-  validates :coment, length: { maximum: 100}
-  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+  validates :coment, length: { maximum: 300}
+
+#validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+#  message: "対応している画像形式ではありません" },
+#size:         { less_than: 5.megabytes,
+#  message:   "5MBより大きいファイルを投稿できません" }
+
+  validates :images,   content_type: { in: %w[image/jpeg image/gif image/png],
   message: "対応している画像形式ではありません" },
-size:         { less_than: 5.megabytes,
+  size:         { less_than: 5.megabytes,
   message:   "5MBより大きいファイルを投稿できません" }
+  validate :image_length
 
   def self.search_for(area1,area2,time,genre)
     Restaurant.where(['area1 Like(?) AND area2 Like(?) AND time Like(?) AND genre Like(?)',"%#{area1}%","%#{area2}%","%#{time}%","%#{genre}%"])
@@ -22,4 +27,11 @@ size:         { less_than: 5.megabytes,
   def self.choose_for(area1,area2,time,genre,user)
     Restaurant.where(['area1 Like(?) AND area2 Like(?) AND time Like(?) AND genre Like(?) AND user_id =?',"%#{area1}%","%#{area2}%","%#{time}%","%#{genre}%",user])
   end 
+
+  def image_length
+    if images.length > 4
+      images.purge
+      errors.add(:images, "は4枚以内にしてください")
+    end
+  end
 end
