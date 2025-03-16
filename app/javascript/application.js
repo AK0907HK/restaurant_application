a702b42226082b5580c4e64639e9df8edde183d9
AK-0907 <101916006+AK-0907@users.noreply.github.com>
2025-03-16 14:10:59 +0900

    Merge pull request #82 from AK-0907/#81
    Merge pull request #80 from AK-0907/#79

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 //import "controllers"
+//import Swiper from "swiper";
+//window.Swiper = Swiper;
+
 import "./custom/menu"
 import "./custom/google_map"
 import "./custom/linked_select"
-import "./custom/swiper"
-import "swiper"
+//import "./custom/swiper"
+//import "swiper"
