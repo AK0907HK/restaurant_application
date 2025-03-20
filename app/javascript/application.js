97d06ead061d780dd1f23e711dc3208c848319f3
AK-0907 <101916006+AK-0907@users.noreply.github.com>
2025-03-20 20:33:23 +0900

    Merge pull request #99 from AK-0907/#98
    Merge pull request #97 from AK-0907/#96

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails";
 
-//import "./custom/menu"
-//import "./custom/google_map"
-//import "./custom/linked_select"
-//import "/google_map"
-//import "/linked_select"
+
+import "./custom/google_map";
+import "./custom/linked_select";
