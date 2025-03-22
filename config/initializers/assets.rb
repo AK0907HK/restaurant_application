e543e8643c7b5b8460cdd409f7624f333c05a4e2
AK-0907 <101916006+AK-0907@users.noreply.github.com>
2025-03-22 14:35:37 +0900

    Merge pull request #104 from AK-0907/#103
    Merge pull request #102 from AK-0907/#101

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "builds")
 
-Rails.application.config.assets.precompile += %w[custom/google_map.js custom/linked_select.js]
+
 
 
 
